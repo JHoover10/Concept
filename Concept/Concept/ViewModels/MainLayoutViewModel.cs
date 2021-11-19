5c1124f983f75f811465cd6d208693b201b9117b
Jeffrey Hoover <Jeffrey.Hoover@publix.com>
2021-11-19 01:19:07 -0500

    create MainLayoutViewModel

---
+++ Concept/Concept/ViewModels/MainLayoutViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Concept.Models;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Concept.ViewModels
+{
+    public class MainLayoutViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private List<ConceptCategory> _conceptCategories = new List<ConceptCategory>();
+
+        public List<ConceptCategory> ConceptCategories
+        {
+            get { return _conceptCategories; }
+            set 
+            { 
+                _conceptCategories = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainLayoutViewModel()
+        {
+
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
