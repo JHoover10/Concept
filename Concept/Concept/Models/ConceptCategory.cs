e06202000423e38d4713b5290b903440c48d314f
Jeffrey Hoover <Jeffrey.Hoover@publix.com>
2021-11-19 23:12:45 -0500

    fix bug with dynamic filter

--- Concept/Concept/Models/ConceptCategory.cs
+++ Concept/Concept/Models/ConceptCategory.cs
@@ -4,24 +4,7 @@
     {
         public string DisplayName { get; set; }
         public string FilePath { get; set; }
-        private bool _enabled;
-        public bool Enabled 
-        {
-            get { return _enabled; }
-
-            set
-            {
-                if (SubCategories != null && SubCategories.Any())
-                {
-                    foreach (ConceptCategory category in SubCategories)
-                    {
-                        category.Enabled = value;
-                    }
-                }
-
-                _enabled = value;
-            }
-        }
+        public bool Enabled { get; set; }
         public List<ConceptCategory> SubCategories { get; set; }
     }
 }
