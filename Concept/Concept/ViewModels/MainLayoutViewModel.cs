57cbe04fbd9d0ea2a2cf7256a57ab89c0651f978
Jeffrey Hoover <Jeffrey.Hoover@publix.com>
2021-11-20 00:55:50 -0500

    refactor how i set concept categories

--- Concept/Concept/ViewModels/MainLayoutViewModel.cs
+++ Concept/Concept/ViewModels/MainLayoutViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using Concept.Models;
+using Newtonsoft.Json;
 using System.ComponentModel;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Runtime.CompilerServices;
 
 namespace Concept.ViewModels
@@ -9,6 +12,7 @@ namespace Concept.ViewModels
         public event PropertyChangedEventHandler PropertyChanged;
 
         private List<ConceptCategory> _conceptCategories = new List<ConceptCategory>();
+        private readonly HttpClient _httpClient;
 
         public List<ConceptCategory> ConceptCategories
         {
@@ -20,110 +24,16 @@ namespace Concept.ViewModels
             }
         }
 
-        public MainLayoutViewModel()
+        public MainLayoutViewModel(HttpClient httpClient)
         {
-            AddConceptCategories();
+            _httpClient = httpClient;
         }
 
-        private void AddConceptCategories()
+        public async Task AddConceptCategories()
         {
-            _conceptCategories.Add(new ConceptCategory()
-            {
-                DisplayName = "Default",
-                FilePath = "default.json",
-                Enabled = true,
-            });
+            List<ConceptCategory> conceptCategories = await _httpClient.GetFromJsonAsync<List<ConceptCategory>>("data/conceptCategories.json");
 
-            _conceptCategories.Add(new ConceptCategory()
-            {
-                DisplayName = "Pokemon",
-                SubCategories = new List<ConceptCategory>()
-                {
-                    new ConceptCategory() 
-                    {
-                        DisplayName = "Generation 1",
-                        FilePath = "pokemon/generation1.json"
-                    },
-                    new ConceptCategory()
-                    {
-                        DisplayName = "Generation 2",
-                        FilePath = "pokemon/generation2.json"
-                    },
-                    new ConceptCategory()
-                    {
-                        DisplayName = "Generation 3",
-                        FilePath = "pokemon/generation3.json"
-                    },
-                    new ConceptCategory()
-                    {
-                        DisplayName = "Generation 4",
-                        FilePath = "pokemon/generation4.json"
-                    },
-                    new ConceptCategory()
-                    {
-                        DisplayName = "Generation 5",
-                        FilePath = "pokemon/generation5.json"
-                    },
-                    new ConceptCategory()
-                    {
-                        DisplayName = "Generation 6",
-                        FilePath = "pokemon/generation6.json"
-                    },
-                    new ConceptCategory()
-                    {
-                        DisplayName = "Generation 7",
-                        FilePath = "pokemon/generation7.json"
-                    },
-                }
-            });
-
-            _conceptCategories.Add(new ConceptCategory()
-            {
-                DisplayName = "Disney Movies",
-                SubCategories = new List<ConceptCategory>()
-                {
-                    new ConceptCategory()
-                    {
-                        DisplayName = "The Golden Era (1937 to 1942)",
-                        FilePath = "disneyMovies/theGoldenEra.json"
-                    },
-                    new ConceptCategory()
-                    {
-                        DisplayName = "The Wartime Era (1942 to 1949)",
-                        FilePath = "disneyMovies/theWartimeEra.json"
-                    },
-                    new ConceptCategory()
-                    {
-                        DisplayName = "The Silver Era (1950 to 1967)",
-                        FilePath = "disneyMovies/theSilverEra.json"
-                    },
-                    new ConceptCategory()
-                    {
-                        DisplayName = "The Bronze Era (1970 to 1977)",
-                        FilePath = "disneyMovies/theBronzeEra.json"
-                    },
-                    new ConceptCategory()
-                    {
-                        DisplayName = "The Dark Ages (1981 to 1988)",
-                        FilePath = "disneyMovies/theDarkAges.json"
-                    },
-                    new ConceptCategory()
-                    {
-                        DisplayName = "The Renaissance Era (1989 to 1999)",
-                        FilePath = "disneyMovies/theRenaissanceEra.json"
-                    },
-                    new ConceptCategory()
-                    {
-                        DisplayName = "The Experimental Era (1999 to 2008)",
-                        FilePath = "disneyMovies/theExperimentalEra.json"
-                    },
-                    new ConceptCategory()
-                    {
-                        DisplayName = "The Revival Era (2009 to Present)",
-                        FilePath = "disneyMovies/theRevivalEra.json"
-                    },
-                }
-            });
+            _conceptCategories.AddRange(conceptCategories);
         }
 
         private void OnPropertyChanged([CallerMemberName] string propertyName = null)
